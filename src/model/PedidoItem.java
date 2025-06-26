358eb7236019d842cf42c22cf7710300fc469735
harckys <lucassilva.cruzs@hotmail.com>
2025-06-26 01:19:54 -0300

    Update PedidoItem.java

--- src/model/PedidoItem.java
+++ src/model/PedidoItem.java
@@ -1,4 +1,25 @@
 package model;
 
 public class PedidoItem {
+    private Item item;
+    private int quantidade;
+    private double subtotal;
+
+    // Getters e Setters
+    public Item getItem() { return item; }
+    public void setItem(Item item) { this.item = item; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+        calcularSubtotal();
+    }
+
+    public double getSubtotal() { return subtotal; }
+
+    public void calcularSubtotal() {
+        if (item != null && quantidade > 0) {
+            this.subtotal = item.getPreco() * quantidade;
+        }
+    }
 }
