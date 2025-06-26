1c5643a6351191cc8063cba76238c6db19fa478e
harckys <lucassilva.cruzs@hotmail.com>
2025-06-26 01:19:32 -0300

    Update Pedido.java

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,4 +1,34 @@
 package model;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 public class Pedido {
+    private int id;
+    private Cliente cliente;
+    private String dataHora;
+    private String status;
+    private double total;
+    private List<PedidoItem> itens;
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public String getDataHora() { return dataHora; }
+    public void setDataHora(String dataHora) { this.dataHora = dataHora; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
+
+    public List<PedidoItem> getItens() { return itens; }
+    public void setItens(List<PedidoItem> itens) { this.itens = itens;
+
+    }
 }
