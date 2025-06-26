490235f25aaa12d539b5d2b8728776586cad849b
harckys <lucassilva.cruzs@hotmail.com>
2025-06-26 01:19:10 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Cliente {
+    private int id;
+    private String nome;
+    private String cpfCnpj;
+    private String email;
+    private String telefone;
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpfCnpj() { return cpfCnpj; }
+    public void setCpfCnpj(String cpfCnpj) { this.cpfCnpj = cpfCnpj; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone;}
+    public String toString() {return nome + " (" + cpfCnpj + ")";}
+
+
 }
