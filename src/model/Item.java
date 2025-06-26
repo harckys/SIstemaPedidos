bdc473f8f1cd8ae2049e46943f00ce1a57eb372e
harckys <lucassilva.cruzs@hotmail.com>
2025-06-26 01:19:22 -0300

    Update Item.java

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Item {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco;}
+    public String toString() {return nome + " - R$" + String.format("%.2f", preco);}
+
 }
