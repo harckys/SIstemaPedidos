0caef7a534131c5c41763f58c00a4728ef4390b9
harckys <lucassilva.cruzs@hotmail.com>
2025-06-26 01:16:00 -0300

    Update ClienteDAO.java

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,4 +1,69 @@
 package dao;
+import model.Cliente;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteDAO {
+    public boolean salvar(Cliente c) {
+        String sql = "INSERT INTO cliente (nome, cpf_cnpj, email, telefone) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, c.getNome());
+            stmt.setString(2, c.getCpfCnpj());
+            stmt.setString(3, c.getEmail());
+            stmt.setString(4, c.getTelefone());
+
+            stmt.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            if (e.getMessage().contains("UNIQUE")) {
+                System.out.println("CPF/CNPJ duplicado.");
+            } else {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
+    public boolean existeCpfCnpj(String cpfCnpj) {
+        String sql = "SELECT id FROM cliente WHERE cpf_cnpj = ?";
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, cpfCnpj);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public List<Cliente> listar() {
+        List<Cliente> lista = new ArrayList<>();
+        String sql = "SELECT * FROM cliente ORDER BY nome";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                c.setId(rs.getInt("id"));
+                c.setNome(rs.getString("nome"));
+                c.setCpfCnpj(rs.getString("cpf_cnpj"));
+                c.setEmail(rs.getString("email"));
+                c.setTelefone(rs.getString("telefone"));
+                lista.add(c);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+
 }
+
