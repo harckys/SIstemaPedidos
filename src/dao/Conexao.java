48e69b522146bb9d0b5f0d4d9bfc671da349c1df
harckys <lucassilva.cruzs@hotmail.com>
2025-06-26 01:16:16 -0300

    Update Conexao.java

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -1,4 +1,12 @@
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
+    private static final String URL = "jdbc:sqlite:banco.db";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
 }
